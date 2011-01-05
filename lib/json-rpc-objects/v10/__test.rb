e3845edc8ebb8bcf78497348f9912562d80f8ec5
Martin Kozák <martinkozak@martinkozak.net>
2011-01-05 23:26:52 +0100

    test/example routines

---
+++ lib/json-rpc-objects/v10/__test.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+require "./request"
+req = JsonRpcObjects::V10::Request::create(:alfa, [:beta], :id => 12345)
+puts req.to_json
+
+require "./response"
+res = JsonRpcObjects::V10::Response::create(true, nil, :id => 12345)
+puts res.to_json
---
+++ lib/json-rpc-objects/v11/__test.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+$:.push("../..")
+
+require "./procedure-call"
+req = JsonRpcObjects::V11::ProcedureCall::create(:alfa, [:beta], :id => 12345, :"$whatever" => false)
+puts req.to_json
+
+require "./error"
+err = JsonRpcObjects::V11::Error::create(200, "some problem")
+
+require "./procedure-return"
+res = JsonRpcObjects::V11::ProcedureReturn::create(nil, err, :id => 12345)
+puts res.to_json
+res = JsonRpcObjects::V11::ProcedureReturn::create(true, nil, :id => 12345)
+puts res.to_json
+
+require "./service-description"
+sdesc = JsonRpcObjects::V11::ServiceDescription::create(:alfa, 100)
+puts sdesc.to_json
+
+require "./service-procedure-description"
+sproc = JsonRpcObjects::V11::ServiceProcedureDescription::create(:some_proc)
+sdesc << sproc
+puts sdesc.to_json
+
+require "./procedure-parameter-description"
+sparam1 = JsonRpcObjects::V11::ProcedureParameterDescription::create(:param1, :type => :str)
+sparam2 = JsonRpcObjects::V11::ProcedureParameterDescription::create(:param2, :type => JsonRpcObjects)
+sproc << sparam1
+sproc << sparam2
+puts sdesc.to_json
